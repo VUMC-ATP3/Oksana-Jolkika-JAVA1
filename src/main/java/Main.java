eaeb32bebf229bf73ea457fa7eb30acc2f156ead
Oksana-Jolkina <oksanasan@inbox.lv>
2022-07-08 22:45:15 +0300

    inital commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,72 @@
+import com.sun.source.doctree.SummaryTree;
+
+public class Main {
+    public static void main(String[] args) {
+        int vecumsSuns = 3;
+        String vards = "Pirats";
+        double svarsSuns =15.8; //daiļskaitlis
+        float augumsSuņs = 60.21F;
+        boolean irIzsalcis=true;
+        char dzimumsSuns = 'V';
+
+
+
+        System.out.println("Mans suns");
+        System.out.println("Vecums");
+        System.out.println(vecumsSuns);
+        System.out.println("Vārds");
+        System.out.println(vards);
+        System.out.println("Svars");
+        System.out.println(svarsSuns+"kg");
+        System.out.println("Augums");
+        System.out.println(augumsSuņs);
+        System.out.println("Vai mans suns ir izsalcis?");
+        System.out.println(irIzsalcis);
+        System.out.println("Dzimums");
+        System.out.println(dzimumsSuns);
+
+        int vecumsKakis = 12;
+        String vardsKaķis = "Umka    ";
+
+        System.out.println("Mans kaķis");
+        System.out.println("vecums");
+        System.out.println(vecumsKakis);
+        System.out.println("Vārds");
+        System.out.println(vardsKaķis);
+
+        System.out.println("Kaķa un suņa kopējais vecums ir");
+        System.out.println(vecumsKakis+vecumsSuns);
+        System.out.println(vardsKaķis+ "" + vards);
+
+
+        System.out.println("Hello Oksana");
+
+        int summa;
+        double dalijums;
+        int starpiba;
+        int reizinajums;
+        int x = 5;
+        int y = 10;
+        summa = x + y;
+        starpiba = x - y;
+        reizinajums = x * y;
+
+
+        double z = 5;
+        double q = 10;
+        double w = 12;
+
+        dalijums = z / q;
+
+        System.out.println("Summa " + summa);
+        System.out.println("Starpība " + starpiba);
+        System.out.println("Reizinājums " + reizinajums);
+        System.out.println("Dalījums " + dalijums);
+
+        int currentEiro;
+        System.out.println("currentEiro");
+
+
+
+    }
+}
