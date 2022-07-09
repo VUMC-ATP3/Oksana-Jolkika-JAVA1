bf29509a789caab00cbe1819fc9405e0e5241536
Oksana-Jolkina <oksanasan@inbox.lv>
2022-07-09 11:30:54 +0300

    Add home work JAVA1

--- src/main/java/homework/HomeWork.java
+++ src/main/java/homework/HomeWork.java
@@ -2,6 +2,47 @@ package homework;
 
 public class HomeWork {
     public static void main(String[] args) {
+        String galvaspilseta = "Parīze";
+        String oficialaValoda = "Franču";
+        double iedzivotajuSkaits = 67.39;
+        String miljoni = "miljoni";
+        float platiba = 674.843F;
+        String valuta = "€";
+        boolean vaiIrEsDalibvalsts = true;
+
+        System.out.println("FRANCIJA");
+        System.out.println("Galvaspilsēta: " + galvaspilseta);
+        System.out.println("Oficiala Valoda: " + oficialaValoda);
+        System.out.println("Iedzīvotāju Skaits: " + iedzivotajuSkaits +  miljoni );
+        System.out.println("Platība: " + platiba+ "km^2");
+        System.out.println("Valūta: " + valuta);
+        System.out.println("Vai ir ES dalībvalsts: " + vaiIrEsDalibvalsts);
+
+
+        int summa;
+        int dalijums;
+        int starpiba;
+        int reizinajums;
+        int x = 20;
+        int y = 2;
+        summa = x + y;
+        starpiba = x - y;
+        reizinajums = x * y;
+        dalijums =x / y;
+
+
+        System.out.println("Summa " + summa);
+        System.out.println("Starpība " + starpiba);
+        System.out.println("Reizinājums " + reizinajums);
+        System.out.println("Dalījums " + dalijums);
+
+
+
+
+
+
+
+
 
     }
 }
